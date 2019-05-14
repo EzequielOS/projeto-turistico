910366499925194711041fee36ebe7238600f3e0
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-05-14 16:36:40 -0300

    Inicia o repositorio com .gitignore

---
+++ sistema-turismo/src/main/java/br/com/dominio/turismo/sistema_turismo/App.java
@@ -0,0 +1,13 @@
+package br.com.dominio.turismo.sistema_turismo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
